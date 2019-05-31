8a79cff393166dfa83e162897993da3775589bea
Jonathan Rusk <jrusk@chromis.com>
2019-05-31 08:36:05 -0700

    adjust config for Now serverless

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 //const withSass = require('@zeit/next-sass')
 //module.exports = withSass()
 
-const {PHASE_PRODUCTION_SERVER} =
+const { PHASE_PRODUCTION_SERVER } =
   process.env.NODE_ENV === 'development'
     ? {} // We're never in "production server" phase when in development mode
     : !process.env.NOW_REGION
     ? require('next/constants') // Get values from `next` package when building locally
     : require('next-server/constants'); // Get values from `next-server` package when building on now v2
 
-module.exports = (phase, {defaultConfig}) => {
+module.exports = (phase, { defaultConfig }) => {
+  defaultConfig.target = 'serverless';
+
   if (phase === PHASE_PRODUCTION_SERVER) {
     // Config used to run in production.
     return {};
