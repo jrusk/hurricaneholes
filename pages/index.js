080dee59d6fdd2ff13880788b803a1c26954c929
Jonathan Rusk <jrusk@chromis.com>
2021-08-17 16:23:33 -0700

    upgrade to Next 11, replace Bulma with Grommet (#14)

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import React from "react";
+import Image from "next/image";
+import { Anchor, Heading, Paragraph } from "grommet";
 
 const Index = () => (
   <div>
-    <h1 className="title">Welcome to Hurricane Holes</h1>
-    <img src="jeep.jpg" className="headerImage" />
-    <p>We drill holes in dirt.</p>
+    <Heading>Welcome to Hurricane Holes</Heading>
+    <Image src="/jeep.jpg" alt="Jeep photo" width="300" height="200" />
+    <Paragraph>We drill holes in dirt.</Paragraph>
     <br />
-    <h3 className="subtitle">Contact</h3>
+    <Heading level="3">Contact</Heading>
     <p>
       <strong>Address:</strong> 1548 W. Northern Ave. Phoenix, AZ 85021
     </p>
@@ -18,9 +19,9 @@ const Index = () => (
     </p>
     <p>
       <strong>Email:</strong>{" "}
-      <a href="mailto:info@hurricaneholes.com" target="_top">
+      <Anchor href="mailto:info@hurricaneholes.com" target="_top">
         info@hurricaneholes.com
-      </a>
+      </Anchor>
     </p>
   </div>
 );
